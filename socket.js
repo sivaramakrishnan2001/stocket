eda4e712dbfa6433424394fade4e3a934f32cfa2
Sivaramakrishnan.S <sivaramakrishnan.s@marketsimplified.com>
2023-08-08 15:09:06 +0530

    new test

--- socket.js
+++ socket.js
@@ -37,7 +37,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const httpServer = createServer(app);
+// const httpServer = createServer(app);
 
 // new
 // const io = new Server(httpServer, {
@@ -47,7 +47,7 @@ const httpServer = createServer(app);
 //     }
 // });
 
-const io = new Server(httpServer);
+// const io = new Server(httpServer);
 
 // const server = createServer(app);
 // const io = socketIO(server);
@@ -56,48 +56,50 @@ const io = new Server(httpServer);
 
 // ==================================================================
 
-var users = [];
+// var users = [];
 
 
-io.on("connection", (socket) => {
-
-
-
-    socket.on('addUser', (userId) => {
-        addUser(userId, socket.id);
-        console.log("users", users);
-        io.emit('getUsers', users);
-    })
-
-    socket.on('sendMessage', ({ senderId, receiverId, text }) => {
-        const user = getUser(receiverId);
-        io.to(user?.socketId).emit("getMessage",
-            {
-                senderId,
-                text
-            }
-        )
-    })
-
-    socket.on("disconnect", () => {
-        console.log("a user disconnected!");
-
-    });
-});
-
-const addUser = (userId, socketId) => {
-    !users.some((user) => user.userId === userId) &&
-        users.push({ userId, socketId });
-}
-
-const getUser = (userId) => {
-    return users.find((user) => user.userId === userId);
-}
+// io.on("connection", (socket) => {
 
 
 
+//     socket.on('addUser', (userId) => {
+//         addUser(userId, socket.id);
+//         console.log("users", users);
+//         io.emit('getUsers', users);
+//     })
 
-httpServer.listen(process.env.PORT, () => {
+//     socket.on('sendMessage', ({ senderId, receiverId, text }) => {
+//         const user = getUser(receiverId);
+//         io.to(user?.socketId).emit("getMessage",
+//             {
+//                 senderId,
+//                 text
+//             }
+//         )
+//     })
+
+//     socket.on("disconnect", () => {
+//         console.log("a user disconnected!");
+
+//     });
+// });
+
+// const addUser = (userId, socketId) => {
+//     !users.some((user) => user.userId === userId) &&
+//         users.push({ userId, socketId });
+// }
+
+// const getUser = (userId) => {
+//     return users.find((user) => user.userId === userId);
+// }
+
+
+
+
+app.listen(process.env.PORT, () => {
     console.log(`server started http://localhost:${process.env.PORT}`);
 });
+
+
 
